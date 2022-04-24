a18e6d363d98bbd262eec24c737fece4ffd9b0de
Handra Hilman Yunus <46528042+handrahilman@users.noreply.github.com>
2022-04-24 17:06:11 +0700

    revised query

--- sql/query.py
+++ sql/query.py
@@ -2,14 +2,14 @@ def create_table_dim(schema):
   query = f"""
   CREATE TABLE IF NOT EXISTS {schema}.dim_location (
     location_code text primary key,
-    location text;
+    location text);
   CREATE TABLE IF NOT EXISTS {schema}.dim_province (
       location_code text primary key,
-      location text;
-      location_level text;
-  CREATE TABLE IF NOT EXISTS {schema}.dim_population_detail (
+      location text,
+      location_level text);
+  CREATE TABLE IF NOT EXISTS {schema}.dim_province_population_level (
       location_code text primary key,
-      population_level text;
+      population_level text);
   CREATE TABLE IF NOT EXISTS {schema}.dim_case (
       id SERIAL primary key,
       status_name text,
